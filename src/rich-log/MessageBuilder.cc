7afcf984a9561c58885560616fb0aa2195716093
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2020-01-09 09:52:28 +0100

    Fix win32 compilation

--- src/rich-log/MessageBuilder.cc
+++ src/rich-log/MessageBuilder.cc
@@ -13,12 +13,7 @@ MessageBuilder& MessageBuilder::operator()(const char* fmt, ...)
         {
             va_list args;
             va_start(args, fmt);
-
-#ifdef CC_OS_WINDOWS
-            std::vsprintf_s(formatted, 1024, fmt, args);
-#elif defined(CC_OS_LINUX)
             std::vsnprintf(formatted, 1024, fmt, args);
-#endif
             va_end(args);
         }
 
