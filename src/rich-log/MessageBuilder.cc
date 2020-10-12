6d179b1f4e91cfb26cc3d6f00ca014efb0a6e91f
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2020-10-12 16:31:38 +0200

    moved whitelisting to experimental namespace

--- src/rich-log/MessageBuilder.cc
+++ src/rich-log/MessageBuilder.cc
@@ -3,13 +3,17 @@
 #include <clean-core/assert.hh>
 #include <clean-core/vector.hh>
 
+#include <rich-log/experimental.hh>
 #include <rich-log/logger.hh>
 
 using namespace rlog;
 
-static cc::vector<cc::unique_function<bool(cc::string_view domain, cc::string_view msg)>> sWhitelistFilters;
+static cc::unique_function<bool(cc::string_view domain, cc::string_view msg)> sWhitelistFilter;
 
-void rlog::add_whitelist_filter(cc::unique_function<bool(cc::string_view, cc::string_view)> filter) { sWhitelistFilters.push_back(cc::move(filter)); }
+void rlog::experimental::set_whitelist_filter(cc::unique_function<bool(cc::string_view, cc::string_view)> filter)
+{
+    sWhitelistFilter = cc::move(filter);
+}
 
 void MessageBuilder::append_formatted(cc::string_view fmt, cc::span<cc::string const> args)
 {
@@ -34,17 +38,8 @@ void MessageBuilder::append_formatted(cc::string_view fmt, cc::span<cc::string c
 MessageBuilder::~MessageBuilder()
 {
     if (_must_be_whitelisted)
-    {
-        auto ok = false;
-        for (auto const& f : sWhitelistFilters)
-            if (f(_domain.value ? _domain.value : "", _msg))
-            {
-                ok = true;
-                break;
-            }
-        if (!ok)
+        if (!sWhitelistFilter(_domain.value ? _domain.value : "", _msg))
             return; // was not whitelisted
-    }
 
     // TODO: send message to logger
     // DEBUG: console for now
