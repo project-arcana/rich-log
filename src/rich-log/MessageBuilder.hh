42d183e6264c3a37366202194d7542008f3d9a8f
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2020-11-01 04:53:02 +0100

    Adapt to cc change

--- src/rich-log/MessageBuilder.hh
+++ src/rich-log/MessageBuilder.hh
@@ -56,7 +56,7 @@ public:
         }
     }
 
-    void printf(char const* fmt, ...) CC_PRINTF_FUNC(2, 3);
+    void printf(char const* fmt, ...) CC_PRINTF_FUNC(2);
 
     MessageBuilder& operator()() { return *this; }
 
