d2a1b9df589a6236bb47d251557b01efad6eca53
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2022-06-26 11:44:45 +0200

    removed duplicated test

--- tests/logger.cc
+++ tests/logger.cc
@@ -5,22 +5,6 @@
 
 RICH_LOG_DECLARE_DOMAIN(Test);
 
-TEST("basic logging", disabled) // call directly to run this test (it will print to console)
-{
-    rlog::enable_win32_colors();
-    rlog::set_current_thread_name("td#0");
-
-    LOG("hello world!");
-    LOG("{1} {0}!", "world", "hello");
-
-    LOGD(Test, Warning, "'tis a warning, mate");
-    LOGD(Test, Info, "information follows at %s", 12345);
-
-    LOGD(Test, Error, "an error occured"); // debugger should break here
-
-    CHECK(true); // disable warning
-}
-
 TEST("logging capture")
 {
     cc::string msg;
