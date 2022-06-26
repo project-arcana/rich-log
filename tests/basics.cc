4ee99d1d44c4b2b0703642be7b87324b1deb55ac
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2022-06-26 11:07:08 +0200

    logger override tests

--- tests/basics.cc
+++ tests/basics.cc
@@ -22,3 +22,58 @@ TEST("basic logging", disabled) // call directly to run this test (it will print
 
     CHECK(true); // disable warning
 }
+
+TEST("logging capture")
+{
+    cc::string msg;
+    auto _ = rlog::scoped_logger_override(
+        [&](rlog::message_ref m, bool& do_break)
+        {
+            msg = m.message;
+            do_break = false;
+        });
+
+    LOG("Hello World");
+    CHECK(msg == "Hello World");
+
+    LOGD(Test, Warning, "warning nr %d", 10);
+    CHECK(msg == "warning nr 10");
+
+    LOGD(Test, Fatal, "%% fatal %%");
+    CHECK(msg == "% fatal %");
+
+    {
+        cc::string msg2;
+        auto _ = rlog::scoped_logger_override([&](rlog::message_ref m, bool&) { msg2 = m.message; });
+
+        LOG("nested msg");
+        CHECK(msg == "% fatal %");
+        CHECK(msg2 == "nested msg");
+    }
+
+    LOG("not nested anymore");
+    CHECK(msg == "not nested anymore");
+}
+
+TEST("default logger")
+{
+    cc::string msg;
+    rlog::set_global_default_logger([&](rlog::message_ref m, bool&) { msg = m.message; });
+
+    LOG("message A");
+    CHECK(msg == "message A");
+
+    {
+        cc::string msg2;
+        auto _ = rlog::scoped_logger_override([&](rlog::message_ref m, bool&) { msg2 = m.message; });
+
+        LOG("local msg");
+        CHECK(msg == "message A");
+        CHECK(msg2 == "local msg");
+    }
+
+    LOG("not nested anymore");
+    CHECK(msg == "not nested anymore");
+
+    rlog::set_global_default_logger({});
+}
